de104a252ca5f88b1c6b26999e728d85c35a9fd9
hnrie <140860556+hnrie@users.noreply.github.com>
2025-11-14 12:13:40 +0700

    Integrate git workspace, settings, and account flows

--- Xynor UI/Xynor UI/preload.js
+++ Xynor UI/Xynor UI/preload.js
@@ -3,6 +3,7 @@ const { contextBridge, ipcRenderer } = require('electron');
 contextBridge.exposeInMainWorld('electronapi', {
   openfile: () => ipcRenderer.invoke('dialog:openfile'),
   savefile: (content) => ipcRenderer.invoke('dialog:savefile', content),
+  writefile: (filepath, content) => ipcRenderer.invoke('file:write', filepath, content),
   closewindow: () => ipcRenderer.send('window:close'),
   minimizewindow: () => ipcRenderer.send('window:minimize'),
   maximizewindow: () => ipcRenderer.send('window:maximize'),
@@ -10,5 +11,11 @@ contextBridge.exposeInMainWorld('electronapi', {
   hyperionattach: () => ipcRenderer.invoke('hyperion:attach'),
   hyperionexecute: (code) => ipcRenderer.invoke('hyperion:execute', code),
   startterminalserver: () => ipcRenderer.invoke('terminal:start'),
-  onterminaldata: (callback) => ipcRenderer.on('terminal:data', (_event, data) => callback(data))
+  onterminaldata: (callback) => ipcRenderer.on('terminal:data', (_event, data) => callback(data)),
+  opengitrepo: () => ipcRenderer.invoke('dialog:opengitrepo'),
+  listgitfiles: (repopath) => ipcRenderer.invoke('git:listfiles', repopath),
+  readgitfile: (repopath, relativepath) => ipcRenderer.invoke('git:readfile', repopath, relativepath),
+  getgituser: () => ipcRenderer.invoke('git:getuser'),
+  setgituser: (name, email) => ipcRenderer.invoke('git:setuser', name, email),
+  gitlog: (repopath, limit) => ipcRenderer.invoke('git:log', repopath, limit)
 });
